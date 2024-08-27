277796b1bcaec83b42730ca2dd492366a7ec31ff
PabloGuzman <pabloguzmandeveloper@gmail.com>
2024-08-26 23:09:36 -0300

    feat: create footer v1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { AddBox } from '@mui/icons-material'
+import { Button } from '@mui/material'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +18,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>MATCHMAKING</h1>
+      <AddBox></AddBox>
+      <Button>Test entry point</Button>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
