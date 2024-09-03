6819e4cba8af4fe17511ca656b07c120f06c59ff
DiegoAH23 <diegoaugustoh23@gmail.com>
2024-09-03 00:15:02 -0400

    feat(frontend/src/router): creating the login path and creating an account

--- frontend/src/router/RouterProvider.tsx
+++ frontend/src/router/RouterProvider.tsx
@@ -1,9 +1,21 @@
-import { createBrowserRouter } from "react-router-dom";
-import HomeScreen from "../screens/HomeScreen/HomeScreen";
+import { createBrowserRouter } from 'react-router-dom'
+import HomeScreen from '../screens/HomeScreen/HomeScreen'
+import LoginScreen from '../screens/LoginScreen/LoginScreen'
+import RegisterScreen from '../screens/RegisterScreen/RegisterScreen'
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <HomeScreen />,
   },
-]);
+
+  {
+    path: '/login',
+    element: <LoginScreen />,
+  },
+
+  {
+    path: '/register',
+    element: <RegisterScreen />,
+  },
+])
