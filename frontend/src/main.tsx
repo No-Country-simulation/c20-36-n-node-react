98edd795ea8f63060d87e412abfd2db7a03db566
Pablo Guzmán <99220878+pabloguzmandeveloper@users.noreply.github.com>
2024-09-05 21:23:09 -0300

    Revert "feat: create footer v1 (#44)"
    This reverts commit 6ad0a6283397f1055f6ed7fbb706793ca2e34c6d.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,10 @@
-import { createRoot } from "react-dom/client";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import { StrictMode } from "react";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router/RouterProvider.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
--- frontend/src/router/RouterProvider.tsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import HomeScreen from "../screens/HomeScreen/HomeScreen";
-
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomeScreen />,
-  },
-]);
