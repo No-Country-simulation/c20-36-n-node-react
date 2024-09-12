4a1c00abec62b3de3df42c9bfc08ff720912f172
hackanonimous <esi.segurity@gmail.com>
2024-09-12 12:12:23 -0500

    fix: Variables de entorno para deploy

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -3,8 +3,9 @@ const userService = require("../services/userService");
 const catchAsync = require("../utils/catchAsync")
 
 const userGet = catchAsync(async (req, res) => {
-  const response = await userService.userMe();
-  res.json({ msg: response });
+  const {_id}=req.body
+  const response = await userService.userMe(_id);
+  res.status(httpStatus.CREATED).json({ error:false,data: response });
 });
 
 const userMeUpdate = async (req, res) => {
