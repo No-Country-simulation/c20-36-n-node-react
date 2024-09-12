704eab3e84ce7658d336f408755f6b025fe2df63
PabloGuzman <pabloguzmandeveloper@gmail.com>
2024-09-11 22:05:20 -0300

    fix: resolve conficts libs and imports

--- frontend/src/screens/HomeScreen/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen/HomeScreen.tsx
@@ -7,7 +7,6 @@ import { getMainTheme } from '../../theme/getMainTheme'
 export default function HomeScreen() {
   return (
     <ThemeProvider theme={getMainTheme()}>
-      <Navbar />
       <main>
         <Box sx={{ width: '100%', maxWidth: 375 }}>
           <Typography variant="h1" gutterBottom sx={{ color: 'mono.white' }}>
