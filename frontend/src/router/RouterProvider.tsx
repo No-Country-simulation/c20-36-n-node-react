3377b34bf22930e8317903c0ca8b6dd038d2ebf6
PabloGuzman <pabloguzmandeveloper@gmail.com>
2024-09-15 22:42:11 -0300

    feat: create profile screen

--- frontend/src/router/RouterProvider.tsx
+++ frontend/src/router/RouterProvider.tsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom'
 import MainLayout from '../components/organisms/layouts/MainLayout'
+import { ProfileEditForm } from '../forms/ProfileEditForm'
 import { Dashboard } from '../screens/Dashborad/Dashboard'
 import HomeScreen from '../screens/HomeScreen/HomeScreen'
 import LoginScreen from '../screens/LoginScreen/LoginScreen'
-import { Profile } from '../screens/Profile/Profile'
 import RegisterScreen from '../screens/RegisterScreen/RegisterScreen'
 
 export const router = createBrowserRouter([
@@ -14,8 +14,9 @@ export const router = createBrowserRouter([
       { path: '/', element: <HomeScreen /> },
       { path: '/login', element: <LoginScreen /> },
       { path: '/register', element: <RegisterScreen /> },
-      { path: '/dashboard', element: <Dashboard /> },
-      { path: '/dashboard/profile', element: <Profile /> },
+      // { path: '/dashboard', element: <Dashboard /> },
+      { path: '/dashboard/profile', element: <Dashboard /> },
+      { path: '/dashboard/profile', element: <ProfileEditForm /> },
     ],
   },
 ])
--- frontend/src/screens/Profile/Profile.tsx
+++
@@ -1,5 +0,0 @@
-import { Box } from '@mui/material'
-
-export const Profile = () => {
-  return <Box></Box>
-}
